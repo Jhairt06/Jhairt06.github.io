a124a3b70ecd93e174256e1dc15dc5e1485268da
Jhairt06 <136742324+Jhairt06@users.noreply.github.com>
2024-06-13 14:55:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputElement = document.getElementById("text-input");
+const result = document.getElementById("result")
+let reversedInput = ""
+let inputValue = ""
+const button = document.getElementById("check-btn")
+
+
+button.onclick = function() {
+    inputValue = inputElement.value;
+    console.log(inputValue); 
+    reversedInput = inputValue.split("").reverse().join("");
+    console.log(reversedInput)
+    let isPalindrome = reversedInput === inputValue ? true : false; 
+
+    if (isPalindrome) {
+        result.textContent = `${inputElement.value} is a Palindrome`
+    } else {
+        result.textContent = `${inputElement.value} is not a Palindrome`
+    }
+    inputElement.value = ""
+}
+
+
